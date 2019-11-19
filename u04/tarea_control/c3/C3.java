00b6485bda00a2c5524d9a267759d0f9bb272363
Profe - Montoiro Cabada <fran@a23eql00.sanclemente.local>
2019-11-19 18:38:19 +0100

    added tarea_control/c3

---
+++ u04/tarea_control/c3/C3.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class Ada {
+    public static void main(String[] args) {
+        int n=-1, p=-1, j;
+        long faul;
+        Scanner cin = new Scanner(System.in);
+        while(n!=0 && p!=0) {
+            n = cin.nextInt();
+            p = cin.nextInt();
+            if((n>=1 && n<=10) && (p>=1 && p<=10)) {
+                for(j=1, faul=0; j<=n; j++) faul += Math.pow(j, p);
+                System.out.println(faul);
+            }
+        }
+    }
+}
