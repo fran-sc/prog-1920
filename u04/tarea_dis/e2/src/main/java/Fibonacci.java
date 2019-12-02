2caa269a345b3682a98aeb120cb2e43a60003586
fran-sc <54080955+fran-sc@users.noreply.github.com>
2019-12-02 20:50:07 +0100

    Update Fibonacci.java

--- u04/tarea_dis/e2/src/main/java/Fibonacci.java
+++ u04/tarea_dis/e2/src/main/java/Fibonacci.java
@@ -2,24 +2,17 @@ public class Fibonacci {
     public static void showSerie(int n) {
         final int MAX_COLS = 4;
         long ant1 = 1, ant2 = 1, val;
-        int cont = 0, cols = 1;
+        int cont = 0;
 
-        while(cont<n) {
-            cont++;
-            if(cont<3) { 
-                val = 1;
-            }
+        while(cont++<n) {
+            if(cont<3) val = 1;
             else {
                 val = ant1 + ant2;
                 ant2 = ant1;
                 ant1 = val;
             }
             System.out.printf("%-20d", val);
-            if(++cols>MAX_COLS) {
-                System.out.println();
-                cols = 1;
-            }                
+            if(cont%MAX_COLS==0) System.out.println();
         }
-        System.out.println();    
     }
 }
