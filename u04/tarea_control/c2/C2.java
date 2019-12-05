7edcd0b7da299e9d8cf5f8713f755a15aa98cdbd
zeroth <frangmc@yahoo.es>
2019-12-05 11:51:19 +0100

    final revision

--- u04/tarea_control/c2/C2.java
+++ u04/tarea_control/c2/C2.java
@@ -3,10 +3,8 @@ public static void diamante(int n, char c) {
         int m = n/2 +1;
         for(int i=1, sp, j; i<=n; i++) {
             sp = Math.abs(m-i);
-            for(j=1; j<=sp; j++) 
-                System.out.print(" ");
-            for(j=1; j<=(m-sp)*2-1; j++) 
-                System.out.print(c);
+            for(j=1; j<=sp; j++) System.out.print(" ");
+            for(j=1; j<=n-2*sp; j++) System.out.print(c);
             System.out.println();
         }
     }
--- u04/tarea_control/c3/C3.java
+++ u04/tarea_control/c3/C3.java
@@ -2,16 +2,16 @@ import java.util.Scanner;
 
 public class C3 {
     public static void main(String[] args) {
-        int n=-1, p=-1, j;
+        int n, p, i;
         long faul;
         Scanner cin = new Scanner(System.in);
-        while(n!=0 && p!=0) {
+        do {
             n = cin.nextInt();
             p = cin.nextInt();
             if((n>=1 && n<=10) && (p>=1 && p<=10)) {
-                for(j=1, faul=0; j<=n; j++) faul += Math.pow(j, p);
+                for(i=1, faul=0; i<=n; i++) faul += Math.pow(i, p);
                 System.out.println(faul);
             }
-        }
+        } while(n!=0 && p!=0);
     }
 }
