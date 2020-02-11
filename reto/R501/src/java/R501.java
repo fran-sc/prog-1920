476b1349d3344cd9379a7eb0850f03e2eb38c782
Profe - Montoiro Cabada <fran@infol03.sanclemente.local>
2020-02-11 19:03:16 +0100

    modified tarea_risk R501

--- reto/R501/src/java/R501.java
+++ reto/R501/src/java/R501.java
@@ -1,6 +1,7 @@
 import java.util.Scanner;
+import java.util.Arrays;
 
-public class R501 {
+public class Risk {
     private final static int TD = 0;    // Tropas defensa
     private final static int TA = 1;    // Tropas ataque
     private final static int DD = 2;    // Dados defensa
@@ -11,54 +12,41 @@ public class R501 {
         Scanner cin = new Scanner(System.in);
         int[] data = new int[5];
 
-        while(cin.hasNext()) {
+        int i, j, njd, nja, jd[], ja[];
+
+	// Mientras haya casos de prueba
+	while(cin.hasNext()) {
             data[TD] = cin.nextInt();
             data[TA] = cin.nextInt();
             data[DD] = cin.nextInt();
             data[DA] = cin.nextInt();
             data[NO] = cin.nextInt();
 
-            int njd = (data[TD]<=data[DD])?data[TD]:data[DD];
-            int nja = (data[TA]<=data[DA])?data[TA]:data[DA];
-
-            int[] jd = new int[njd];
-            int[] ja = new int[nja];
-
-            int i, j, k, val;
-
+            // Mientras haya oleadas...
             while(data[NO]-->0) {
-                // tiradas defensa
-                for(i=0, j=0; i<njd; i++, j=0) {
-                    val = cin.nextInt();
-                    while(j!=i) {
-                        if(val > jd[j]) {
-                            for(k=i; k>j; k--) jd[k] = jd[k-1];
-                            break;
-                        }
-                        else j++;
-                    }
-                    jd[j] = val;
-                }
-                // tiradas ataque
-                for(i=0, j=0; i<nja; i++, j=0) {
-                    val = cin.nextInt();
-                    while(j!=i) {
-                        if(val > ja[j]) {
-                            for(k=i; k>j; k--) ja[k] = ja[k-1];
-                            break;
-                        }
-                        else j++;
-                    }
-                    ja[j] = val;
-                }     
-                // eval jugadas
-                int fin = (nja<=njd)?nja:njd;
-                for(i=0; i<fin; i++) 
-                    if(ja[i]>jd[i]) data[TD]--;
-                    else data[TA]--;
-
+                // Jugadas de la defensa
                 njd = (data[TD]<=data[DD])?data[TD]:data[DD];
+                jd = new int[njd];
+            
+                // Jugadas del ataque
                 nja = (data[TA]<=data[DA])?data[TA]:data[DA];
+                ja = new int[nja];
+
+                // Tiradas defensa
+                for(i=0; i<njd; i++) jd[i] = cin.nextInt();
+
+                // Tiradas ataque
+                for(i=0; i<nja; i++) ja[i] = cin.nextInt();
+
+                // Evaluar las jugadas
+                // 1. Ordenar
+                Arrays.sort(jd);
+                Arrays.sort(ja);
+            
+	    	// 2. Emparejar (de mayor a menor)
+                for(i=njd-1, j=nja-1; i>=0 && j>=0; i--, j--) 
+                    if(ja[j]>jd[i]) data[TD]--;
+                    else data[TA]--;
             }
             System.out.println(data[TD] + " " + data[TA]);
         }
