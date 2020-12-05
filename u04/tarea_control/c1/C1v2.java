a0b0e416a378de89da689733512c68e6954d71c6
zeroth <frangmc@yahoo.es>
2020-12-05 11:20:27 +0100

    updated u04/tarea_control/c1

--- u04/tarea_control/c1/C1v2.java
+++ u04/tarea_control/c1/C1v2.java
@@ -1,8 +1,8 @@
 public class C1v2 {
     public static void main(String[] args) {
         for(int i=0; i<100; i++) {
-            if((i%10==0)&&(i>0)) System.out.println();
             System.out.printf("%04d  ", i*i);
+            if((i+1)%10==0) System.out.println();
         }
     }
 }
